7d8bfad594331eeba0b6e6fa92d519730cffacc9
User <user@Users-iMac-Pro.home>
2022-08-08 15:49:50 +0500

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { Refine } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  Layout,
+  ReadyPage,
+  ErrorComponent,
+  LoginPage,
+} from "@pankod/refine-antd";
+import "@pankod/refine-antd/dist/styles.min.css";
+import routerProvider from "@pankod/refine-react-location";
+import dataProvider, { GraphQLClient } from "@pankod/refine-graphql";
+import { RefineKbarProvider } from "@pankod/refine-kbar";
+import { useTranslation } from "react-i18next";
+import { OffLayoutArea } from "components/offLayoutArea";
+import { Header } from "components/layout";
+import { authProvider } from "./authProvider";
+const API_URL = "https://your-graphql-url/graphql";
+
+const client = new GraphQLClient(API_URL);
+const gqlDataProvider = dataProvider(client);
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const i18nProvider = {
+    translate: (key: string, params: object) => t(key, params),
+    changeLocale: (lang: string) => i18n.changeLanguage(lang),
+    getLocale: () => i18n.language,
+  };
+
+  return (
+    <RefineKbarProvider>
+      <Refine
+        notificationProvider={notificationProvider}
+        Layout={Layout}
+        ReadyPage={ReadyPage}
+        catchAll={<ErrorComponent />}
+        routerProvider={routerProvider}
+        dataProvider={gqlDataProvider}
+        authProvider={authProvider}
+        LoginPage={LoginPage}
+        OffLayoutArea={OffLayoutArea}
+        i18nProvider={i18nProvider}
+        Header={Header}
+      />
+    </RefineKbarProvider>
+  );
+}
+
+export default App;

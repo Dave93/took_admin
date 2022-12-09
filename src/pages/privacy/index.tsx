e1329ff4b1a2451540b9bf35ba16c925cf82eb1d
Davron <yul.davron.93@gmail.com>
2022-12-09 18:37:54 +0500

    ggg

---
+++ src/pages/privacy/index.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "@pankod/refine-antd";
+
+const PrivacyPage = () => {
+  return (
+    <Layout>
+      <h1>Privacy</h1>
+      <p>Privacy page</p>
+    </Layout>
+  );
+};
+
+export default PrivacyPage;

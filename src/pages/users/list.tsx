42cb9b3f07dde1b538ee6e2c2e392459ec67b359
Davr <davr@Davrs-NewiMac.home>
2023-02-01 11:41:57 +0500

    users filters

--- src/pages/users/list.tsx
+++ src/pages/users/list.tsx
@@ -12,6 +12,10 @@ import {
   Button,
   Row,
   Col,
+  useDrawerForm,
+  InputNumber,
+  Edit,
+  Drawer,
 } from "@pankod/refine-antd";
 import {
   CrudFilters,
@@ -22,10 +26,13 @@ import {
 import { client } from "graphConnect";
 import { gql } from "graphql-request";
 import { chain } from "lodash";
-
-import { IRoles, ITerminals, IUsers } from "interfaces";
+import { EditOutlined } from "@ant-design/icons";
+import { IRoles, ITerminals, IUsers, IWorkSchedules } from "interfaces";
 import { defaultDateTimeFormat } from "localConstants";
 import { useEffect, useState } from "react";
+import { drive_type, user_status } from "interfaces/enums";
+import FileUploaderMultiple from "components/file_uploader/multiple";
+import * as gqlb from "gql-query-builder";
 
 const OnlineStatus = ({ value }: { value: boolean }) => {
   return (
@@ -46,7 +53,8 @@ export const UsersList: React.FC = () => {
   }>();
   const [terminals, setTerminals] = useState<any[]>([]);
   const [roles, setRoles] = useState<IRoles[]>([]);
-  const { tableProps, searchFormProps } = useTable<
+  const [work_schedules, setWorkSchedules] = useState<any[]>([]);
+  const { tableProps, searchFormProps, filters, setFilters } = useTable<
     IUsers,
     HttpError,
     {
@@ -201,6 +209,64 @@ export const UsersList: React.FC = () => {
     },
   });
 
+  const {
+    drawerProps,
+    formProps,
+    show,
+    close,
+    saveButtonProps,
+    deleteButtonProps,
+    id,
+  } = useDrawerForm<IUsers>({
+    action: "edit",
+    resource: "users",
+    redirect: false,
+    metaData: {
+      fields: [
+        "id",
+        "first_name",
+        "last_name",
+        "created_at",
+        "drive_type",
+        "car_model",
+        "car_number",
+        "card_name",
+        "card_number",
+        "phone",
+        "latitude",
+        "longitude",
+        "status",
+        "max_active_order_count",
+        "doc_files",
+        {
+          users_terminals: [
+            {
+              terminals: ["id", "name"],
+            },
+          ],
+        },
+        {
+          users_work_schedules: [
+            {
+              work_schedules: ["id", "name"],
+            },
+          ],
+        },
+        {
+          users_roles_usersTousers_roles_user_id: [
+            {
+              roles: ["id", "name"],
+            },
+          ],
+        },
+      ],
+      pluralize: true,
+      requestHeaders: {
+        Authorization: `Bearer ${identity?.token.accessToken}`,
+      },
+    },
+  });
+
   const getAllFilterData = async () => {
     const query = gql`
       query {
@@ -217,11 +283,20 @@ export const UsersList: React.FC = () => {
             name
           }
         }
+        workSchedules {
+          id
+          name
+          organization {
+            id
+            name
+          }
+        }
       }
     `;
-    const { roles, cachedTerminals } = await client.request<{
+    const { roles, cachedTerminals, workSchedules } = await client.request<{
       roles: IRoles[];
       cachedTerminals: ITerminals[];
+      workSchedules: IWorkSchedules[];
     }>(query, {}, { Authorization: `Bearer ${identity?.token.accessToken}` });
     var terminalRes = chain(cachedTerminals)
       .groupBy("organization.name")
@@ -233,6 +308,18 @@ export const UsersList: React.FC = () => {
         };
       })
       .value();
+    var workScheduleResult = chain(workSchedules)
+      .groupBy("organization.name")
+      .toPairs()
+      .map(function (item) {
+        return {
+          name: item[0],
+          children: item[1],
+        };
+      })
+      .value();
+
+    setWorkSchedules(workScheduleResult);
     setTerminals(terminalRes);
     setRoles(roles);
   };
@@ -365,8 +452,8 @@ export const UsersList: React.FC = () => {
               </div>
             )}
           />
-          <Table.Column dataIndex="latitude" title="Широта" />
-          <Table.Column dataIndex="longitude" title="Долгота" />
+          {/* <Table.Column dataIndex="latitude" title="Широта" />
+          <Table.Column dataIndex="longitude" title="Долгота" /> */}
           <Table.Column dataIndex="app_version" title="Версия приложения" />
           <Table.Column
             dataIndex="created_at"
@@ -385,13 +472,348 @@ export const UsersList: React.FC = () => {
             render={(_text, record): React.ReactNode => {
               return (
                 <Space>
-                  <EditButton size="small" recordItemId={record.id} hideText />
+                  <Button
+                    size="small"
+                    icon={<EditOutlined />}
+                    onClick={() => show(record.id)}
+                  />
                   <ShowButton size="small" recordItemId={record.id} hideText />
                 </Space>
               );
             }}
           />
         </Table>
+        <Drawer {...drawerProps} width={800}>
+          <Edit
+            canDelete={false}
+            saveButtonProps={{
+              disabled: saveButtonProps.disabled,
+              loading: saveButtonProps.loading,
+              onClick: async () => {
+                try {
+                  let values: any = await formProps.form?.validateFields();
+                  let users_terminals = values.users_terminals;
+                  let work_schedules = values.users_work_schedules;
+                  let roles = values.users_roles_usersTousers_roles_user_id;
+                  delete values.users_terminals;
+                  delete values.users_work_schedules;
+                  delete values.users_roles_usersTousers_roles_user_id;
+
+                  if (
+                    typeof work_schedules === "object" &&
+                    work_schedules.length > 0 &&
+                    work_schedules[0].work_schedules
+                  ) {
+                    work_schedules = work_schedules.map(
+                      (item: any) => item.work_schedules.id
+                    );
+                  }
+
+                  if (
+                    typeof users_terminals === "object" &&
+                    users_terminals.length > 0 &&
+                    users_terminals[0].terminals
+                  ) {
+                    users_terminals = users_terminals.map(
+                      (item: any) => item.terminals.id
+                    );
+                  }
+
+                  if (
+                    typeof roles === "object" &&
+                    roles.length > 0 &&
+                    roles[0].roles
+                  ) {
+                    roles = roles.map((item: any) => item.roles.id)[0];
+                  }
+
+                  let createQuery = gql`
+                    mutation (
+                      $data: usersUpdateInput!
+                      $where: usersWhereUniqueInput!
+                    ) {
+                      updateUser(data: $data, where: $where) {
+                        id
+                      }
+                    }
+                  `;
+                  let { updateUser } = await client.request<{
+                    updateUser: IUsers;
+                  }>(
+                    createQuery,
+                    {
+                      data: values,
+                      where: { id },
+                    },
+                    { Authorization: `Bearer ${identity?.token.accessToken}` }
+                  );
+                  if (updateUser) {
+                    let { query, variables } = gqlb.mutation([
+                      {
+                        operation: "linkUserToRoles",
+                        variables: {
+                          userId: {
+                            value: updateUser.id,
+                            required: true,
+                          },
+                          roleId: {
+                            value: roles,
+                            required: true,
+                          },
+                        },
+                        fields: ["user_id"],
+                      },
+                      {
+                        operation: "linkUserToWorkSchedules",
+                        variables: {
+                          userId: {
+                            value: updateUser.id,
+                            required: true,
+                          },
+                          workScheduleId: {
+                            value: work_schedules,
+                            type: "[String!]",
+                            required: true,
+                          },
+                        },
+                        fields: ["count"],
+                      },
+                      {
+                        operation: "linkUserToTerminals",
+                        variables: {
+                          userId: {
+                            value: updateUser.id,
+                            required: true,
+                          },
+                          terminalId: {
+                            value: users_terminals,
+                            type: "[String!]",
+                            required: true,
+                          },
+                        },
+                        fields: ["count"],
+                      },
+                    ]);
+                    await client.request(query, variables, {
+                      Authorization: `Bearer ${identity?.token.accessToken}`,
+                    });
+                    close();
+                    if (filters) {
+                      const localFilters = [...filters];
+                      setFilters([]);
+                      setFilters(localFilters);
+                    }
+                  }
+                } catch (error) {}
+              },
+            }}
+            deleteButtonProps={deleteButtonProps}
+            recordItemId={id}
+            title="Редактирование разрешения"
+          >
+            <Form {...formProps} layout="vertical">
+              <Row gutter={16}>
+                <Col span={12}>
+                  <Form.Item
+                    label="Статус"
+                    name="status"
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                  >
+                    <Select>
+                      {Object.keys(user_status).map((status: string) => (
+                        <Select.Option key={status} value={status}>
+                          {tr(`users.status.${status}`)}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                    label="Роль"
+                    name="users_roles_usersTousers_roles_user_id"
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                    getValueProps={(value) => {
+                      return {
+                        value: value?.map((item: any) => item.roles.id),
+                      };
+                    }}
+                  >
+                    <Select>
+                      {roles.map((role: IRoles) => (
+                        <Select.Option key={role.id} value={role.id}>
+                          {role.name}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col span={12}>
+                  <Form.Item
+                    label="Имя"
+                    name="first_name"
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                    label="Фамилия"
+                    name="last_name"
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col span={12}>
+                  <Form.Item label="Телефон" name="phone">
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item label="Тип доставки" name="drive_type">
+                    <Select>
+                      {Object.keys(drive_type).map((type: string) => (
+                        <Select.Option key={type} value={type}>
+                          {tr(`deliveryPricing.driveType.${type}`)}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col span={12}>
+                  <Form.Item
+                    label="Филиалы"
+                    name="users_terminals"
+                    getValueProps={(value) => {
+                      return {
+                        value: value?.map((item: any) => item.terminals.id),
+                      };
+                    }}
+                  >
+                    <Select mode="multiple">
+                      {terminals.map((terminal: any) => (
+                        <Select.OptGroup
+                          key={terminal.name}
+                          label={terminal.name}
+                        >
+                          {terminal.children.map((terminal: ITerminals) => (
+                            <Select.Option
+                              key={terminal.id}
+                              value={terminal.id}
+                            >
+                              {terminal.name}
+                            </Select.Option>
+                          ))}
+                        </Select.OptGroup>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                    label="Рабочие графики"
+                    name="users_work_schedules"
+                    getValueProps={(value) => {
+                      return {
+                        value: value?.map(
+                          (item: any) => item.work_schedules.id
+                        ),
+                      };
+                    }}
+                  >
+                    <Select mode="multiple">
+                      {work_schedules.map((work_schedule: any) => (
+                        <Select.OptGroup
+                          key={work_schedule.name}
+                          label={work_schedule.name}
+                        >
+                          {work_schedule.children.map(
+                            (work_schedule: IWorkSchedules) => (
+                              <Select.Option
+                                key={work_schedule.id}
+                                value={work_schedule.id}
+                              >
+                                {work_schedule.name}
+                              </Select.Option>
+                            )
+                          )}
+                        </Select.OptGroup>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col span={12}>
+                  <Form.Item
+                    label="Максимальное количество активных заказов"
+                    name="max_active_order_count"
+                  >
+                    <InputNumber type="number" />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col span={12}>
+                  <Form.Item label="Имя на карте" name="card_name">
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item label="Номер карты" name="card_number">
+                    <Input />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col span={12}>
+                  <Form.Item label="Модель машины" name="car_model">
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item label="Гос. номер машины" name="car_number">
+                    <Input />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Form.Item
+                label="Документы"
+                name="doc_files"
+                style={{
+                  height: 250,
+                }}
+              >
+                {/* @ts-ignore */}
+                <FileUploaderMultiple modelId={id} />
+              </Form.Item>
+            </Form>
+          </Edit>
+        </Drawer>
       </List>
     </>
   );

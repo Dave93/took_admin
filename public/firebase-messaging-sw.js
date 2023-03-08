1a7fdb3684618410ccbe9248737622d17fcbb706
Davr <davr@Davrs-NewiMac.local>
2023-03-08 14:36:09 +0500

    dsad

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -24,6 +24,7 @@ messaging.onBackgroundMessage(function (payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
+    data: payload.notification.data,
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
